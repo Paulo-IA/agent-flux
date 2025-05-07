e3fc163ba6b904463ef194dd87abcea6ad85a28c
Paulo Fernandes <paulor.exe@gmail.com>
2025-05-07 09:22:40 -0300

    temp

--- src/routes/user/UserRoutes.ts
+++ src/routes/user/UserRoutes.ts
@@ -7,7 +7,7 @@ import { isAuth } from "../../middlewares/IsAuth.js";
 export function UserRoutes(app: FastifyInstance) {
   const userController: UserController = appContianer.resolve<UserController>("UserController")
   
-  app.post('/', userController.create.bind(userController))
+  app.post('/', { preHandler: isAuth }, userController.create.bind(userController))
   app.get('/', { preHandler: isAuth }, userController.findMany.bind(userController))
   app.get('/:uniqueId', { preHandler: isAuth }, userController.findUnique.bind(userController))
 }
